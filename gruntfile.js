8cc9ca74c96a8db3c1fe4846c6e1b703f20f9e2e
Belfegorrr <pelichek.iago@gmail.com>
2023-02-27 01:47:58 -0300

    feat: adicionando compressao LESS com grunt

---
+++ gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    less: {
+      development:{
+        files:{
+          'main.css': 'main.less'
+        }
+      },
+      production: {
+        options: {
+          compress: true
+        },
+        files:{
+          'main.min.css': 'main.less'
+        }
+      }
+    },
+    watch: {
+      less:{
+        files: ['src/styles/**/*.less'],
+        task: ['less:development']
+      }
+    }
+  });
+  grunt.registerTask("olaGrunt", function () {
+    const done = this.async();
+    setTimeout(() => {
+      console.log("Ola grunt");
+      done();
+    }, 3000);
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('build', ['less:production']);
+};
